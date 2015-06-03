d4601fb966074840bdee4721485a7ffbccebf484
Vasily Kuznetsov <kvas.it@gmail.com>
2015-06-03 23:52:24 +0200

    Rewrote to simple data constructors without records. Typeclasses make name and descr work universally.

--- Hassets.hs
+++ Hassets.hs
@@ -2,18 +2,28 @@
 -- Haskell port of ost.assets (financial system core).
 --
 
-import Data.Map (Map)
+--                         code   name   descr  precision
+data AssetType = AssetType String String String Integer
+    deriving Show
 
-data AssetType = AssetType
-        { code :: String
-        , name :: String
-        , description :: String
-        , precision :: Integer
-        } deriving Show
+--                                   type      balance
+data HoldingAccount = HoldingAccount AssetType Integer
+    deriving Show
 
-data AssetHolder = AssetHolder
-        { name :: String
-        , description :: String
-        , accounts :: Map AssetType Integer
-        } deriving Show
+--                             name   descr  accounts
+data AssetHolder = AssetHolder String String [HoldingAccount]
+    deriving Show
+
+
+class Named a where
+    name :: a -> String
+    descr :: a -> String
+
+instance Named AssetType where
+    name (AssetType _ n _ _) = n
+    descr (AssetType _ _ d _) = d
+
+instance Named AssetHolder where
+    name (AssetHolder n _ _) = n
+    descr (AssetHolder _ d _) = d
 
