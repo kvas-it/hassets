dfa5276d89a6f034c79e4a09c8cc94f593c676ef
Vasily Kuznetsov <kvas.it@gmail.com>
2015-06-04 00:04:51 +0200

    Added Eq derivation, some accessors and a convertor of balance to a float.

--- Hassets.hs
+++ Hassets.hs
@@ -4,15 +4,15 @@
 
 --                         code   name   descr  precision
 data AssetType = AssetType String String String Integer
-    deriving Show
+    deriving (Show, Eq)
 
---                                   type      balance
+--                                   assetType balance
 data HoldingAccount = HoldingAccount AssetType Integer
-    deriving Show
+    deriving (Show, Eq)
 
 --                             name   descr  accounts
 data AssetHolder = AssetHolder String String [HoldingAccount]
-    deriving Show
+    deriving (Show, Eq)
 
 
 class Named a where
@@ -27,3 +27,9 @@ instance Named AssetHolder where
     name (AssetHolder n _ _) = n
     descr (AssetHolder _ d _) = d
 
+code (AssetType c _ _ _) = c
+precision (AssetType _ _ _ p) = p
+
+assetType (HoldingAccount a _) = a
+balance (HoldingAccount _ b) = b
+fbalance (HoldingAccount t b) = (fromInteger b) / (fromInteger $ 10 ^ (precision t))
