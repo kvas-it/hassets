733c59a853d255f689005423919df85ac0de646d
Vasily Kuznetsov <kvas.it@gmail.com>
2015-06-04 23:34:52 +0200

    Added asset amounts, removed accounts for now and implemented debiting.

--- Hassets.hs
+++ Hassets.hs
@@ -2,16 +2,26 @@
 -- Haskell port of ost.assets (financial system core).
 --
 
+type ID = Integer
+
 --                         code   name   descr  precision
 data AssetType = AssetType String String String Integer
     deriving (Show, Eq)
 
---                                   assetType balance
-data HoldingAccount = HoldingAccount AssetType Integer
+--                             type      amount
+data AssetAmount = AssetAmount AssetType Integer
     deriving (Show, Eq)
 
 --                             name   descr  accounts
-data AssetHolder = AssetHolder String String [HoldingAccount]
+data AssetHolder = AssetHolder String String [AssetAmount]
+    deriving (Show, Eq)
+
+--                             name   descr  source      destination amount
+data Transaction = Transaction String String AssetHolder AssetHolder AssetAmount
+    deriving (Show, Eq)
+
+--             name   descr  transactions
+data Lot = Lot String String [Transaction]
     deriving (Show, Eq)
 
 
@@ -27,9 +37,40 @@ instance Named AssetHolder where
     name (AssetHolder n _ _) = n
     descr (AssetHolder _ d _) = d
 
+instance Named Transaction where
+    name (Transaction n _ _ _ _) = n
+    descr (Transaction _ d _ _ _) = d
+
+instance Named Lot where
+    name (Lot n _ _) = n
+    descr (Lot _ d _) = d
+
+
 code (AssetType c _ _ _) = c
 precision (AssetType _ _ _ p) = p
 
-assetType (HoldingAccount a _) = a
-balance (HoldingAccount _ b) = b
-fbalance (HoldingAccount t b) = (fromInteger b) / (fromInteger $ 10 ^ (precision t))
+
+assetType (AssetAmount t _) = t
+amount (AssetAmount _ a) = a
+inverse (AssetAmount t a) = AssetAmount t (-a)
+
+famount :: AssetAmount -> Float
+famount a = (fromInteger amt) / (fromInteger $ 10 ^ prec)
+    where
+        amt = amount a
+        prec = precision $ assetType a
+
+-- helper function to add amount to set of amounts
+debitA amt [] = [amt]
+debitA amt (acc:accs) =
+        if tAmt == tAcc
+            then (AssetAmount tAcc $ aAcc + aAmt):accs
+            else acc:(debitA amt accs)
+    where
+        tAmt = assetType amt
+        aAmt = amount amt
+        tAcc = assetType acc
+        aAcc = amount acc
+
+debit :: AssetAmount -> AssetHolder -> AssetHolder
+debit amt (AssetHolder n d accs) = AssetHolder n d $ debitA amt accs
